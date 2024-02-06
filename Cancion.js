ae4f7230c38e0a90af94e145652a303e3efaa4e6
SilviaFabara <107445419+SilviaFabara@users.noreply.github.com>
2024-02-06 16:21:26 -0500

    met consttructor y getters de cancion
    añadí el contenido de la clase canción

--- Cancion.js
+++ Cancion.js
@@ -1,3 +1,35 @@
-class Cancion{
-  
+
+  class Cancion{
+
+ // Definir el constructor con los parámetros
+  constructor(nombre, artista, genero, año, foto) {
+    // Asignar los valores de los parámetros a los atributos de la instancia
+    this.nombre = nombre;
+    this.artista = artista;
+    this.genero = genero;
+    this.año = año;
+    this.foto = foto;
+  }
+
+  // Definir el método get para obtener el nombre de la canción
+  get nombreCancion() {
+    return this.nombre;
+  }
+// Definir el método get para obtener el artista de la canción
+  get artistaCancion() {
+    return this.artista;
+  }
+// Definir el método get para obtener el género de la canción
+  get generoCancion() {
+    return this.genero;
+  }
+// Definir el método get para obtener el año de la canción
+  get añoCancion() {
+    return this.año;
+  }
+// Definir el método get para obtener la foto de la canción
+  get fotoCancion() {
+    return this.foto;
+  }
 }
+
