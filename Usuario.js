083ef1b9f19e1b55abee816207b8d0884163be1b
SilviaFabara <107445419+SilviaFabara@users.noreply.github.com>
2024-02-06 16:16:34 -0500

    Update Usuario.js
    agregue met constructor y met iniciar sesion en usuario

--- Usuario.js
+++ Usuario.js
@@ -1,9 +1,18 @@
 class Usuario{
-  
+    constructor(nombre, contraseña) {
+        // Asignar los valores de los parámetros a los atributos de la instancia
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+      }
+
+      iniciarSesion(usuarioInput, contrasenaInput) {
+        // Verificar si las credenciales son válidas
+        if (usuarioInput === this.usuario && contrasenaInput === this.contrasena) {
+            return true; // Credenciales válidas
+        } else {
+            return false; // Credenciales inválidas
+        }
+    }
 
 
-
-
-
-  
 }
